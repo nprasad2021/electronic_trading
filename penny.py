d06fa5c9ecfe8ee7cc18347f8e2836141719968d
Neeraj Prasad <neeraj.prasad@sap.com>
2018-07-14 13:03:58 -0700

    normalcommit

--- penny.py
+++ penny.py
@@ -1,3 +1,24 @@
+#### PENNY TRADING STRATEGY
+import time
+
+def write_to_exchange(exchange, obj):
+    json.dump(obj, exchange)
+    exchange.write("\n")
+
+def trade(exchange_response, team_name):
+
+    if exchange_response['type'] == 'book' and exchange_response['symbol'] == 'bond':
+        max_buy_price = interpreter['buy'][0][0]
+        min_sell_price = interpreter['sell'][0][0]
+
+        request = {"type": "add", "order_id": N, "symbol": "SYM", "dir": "BUY", "price": N, "size": N}
+    	write_to_exchange(exchange, {"type": "hello", "team": team_name.upper()})
 
 
-def
+
+
+def main(response_exchange, team_name):
+
+	trade(response_to_exchange, team_name)
+
+
