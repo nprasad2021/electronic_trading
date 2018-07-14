813a4253b362bde0458587e7cd14b750c52f45c7
Neeraj Prasad <neeraj.prasad@sap.com>
2018-07-14 13:31:52 -0700

    normalcommit

---
+++ main.py
@@ -0,0 +1,4 @@
+import penny
+
+trade_object = penny.Penny()
+trade_object.trade()
--- penny.py
+++ penny.py
@@ -1,24 +1,34 @@
 #### PENNY TRADING STRATEGY
-import time
+import random
+import strategy
 
-def write_to_exchange(exchange, obj):
-    json.dump(obj, exchange)
-    exchange.write("\n")
+class Penny(strategy.Strategy):
 
-def trade(exchange_response, team_name, order_id):
+	def trade(self):
+		self.initiate_trade()
 
-    if exchange_response['type'] == 'book' and exchange_response['symbol'] == 'BOND':
-        max_buy_price = interpreter['buy'][0][0]
-        min_sell_price = interpreter['sell'][0][0]
+		while True:
+			response_exchange = self.read_from_exchange(exchange)
+			self.penny_trade(response)
 
-        buy_request = {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", "price": max_buy_price, "size": 5}
-        sell_request = 
-    	write_to_exchange(exchange, request)
+	def penny_trade(self, exchange_response):
+
+	    if exchange_response['type'] == 'book' and exchange_response['symbol'] == 'BOND':
+	        max_buy_price = interpreter['buy'][0][0]
+	        min_sell_price = interpreter['sell'][0][0]
+
+	        self.order_id += 1
+	        buy_request = {"type": "add", "order_id": self.order_id, "symbol": "BOND", "dir": "BUY", "price": max_buy_price, "size": 5}
+	        self.write_to_exchange(buy_request)
+	        print(self.read_from_exchange(exchange))
+
+	        self.order_id += 1
+	        sell_request = {"type": "add", "order_id": self.order_id, "symbol": "BOND", "dir": "SELL", "price": min_sell_price, "size": 5}
+	    	self.write_to_exchange(sell_request)
+	    	print(self.read_from_exchange(exchange))
 
 
 
 
-def main(response_exchange, team_name, order_id):
-	trade(response_to_exchange, team_name, order_id)
 
 
