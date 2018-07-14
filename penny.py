bc19a20641197383530fd115f2014e1208102376
harshita-kaushal <harshita_kaushal@hotmail.com>
2018-07-14 13:34:02 -0700

    simple_bond.py

--- penny.py
+++ penny.py
@@ -10,9 +10,7 @@ def trade(exchange_response, team_name, order_id):
     if exchange_response['type'] == 'book' and exchange_response['symbol'] == 'BOND':
         max_buy_price = interpreter['buy'][0][0]
         min_sell_price = interpreter['sell'][0][0]
-
-        buy_request = {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", "price": max_buy_price, "size": 5}
-        sell_request = 
+        buy_request = {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", "price": max_buy_price, "size": 5}      sell_request = 
     	write_to_exchange(exchange, request)
 
 
