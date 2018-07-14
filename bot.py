b8583019d51db6db245f37e9c895cc5c1c7c9963
Neeraj Prasad <neeraj.prasad@sap.com>
2018-07-14 12:36:22 -0700

    normalcommit

--- bot.py
+++ bot.py
@@ -11,6 +11,9 @@ import sys
 import socket
 import json
 
+import penny
+
+
 # ~~~~~============== CONFIGURATION  ==============~~~~~
 # replace REPLACEME with your team name!
 team_name="TEAMYELLOW"
@@ -44,14 +47,37 @@ def read_from_exchange(exchange):
 
 # ~~~~~============== MAIN LOOP ==============~~~~~
 
-#def interpret_exchange(json_file):
+def interpret_exchange(exchange):
+    interpreter = response_from_exchange(exchange)
+    if interpreter['type'] == 'book' and interpreter['symbol'] == 'bond':
+        max_share_price = interpreter['buy'][0][0]
+
+
+def harshita_bond_strategy():
+    penny.
+
+def neeraj_bond_strategy():
+
+
+
+
 
 
 def main():
+    person = sys.argv[1]
+
     exchange = connect()
     write_to_exchange(exchange, {"type": "hello", "team": team_name.upper()})
     hello_from_exchange = read_from_exchange(exchange)
-    print(hello_from_exchange)
+
+    if person == 'harshita':
+        harshita_bond_strategy(exchange)
+    else:
+        neeraj_bond_strategy()
+    while True:
+        interpret_exchange(exchange)
+        operate()
+
     write_to_exchange(exchange, {"type": "add", "order_id": 1, "symbol": "GOOG", "dir": "BUY", "price": 1000, "size": 20})
     response_from_exchange = read_from_exchange(exchange)
     print("The exchange replied:", response_from_exchange, file=sys.stderr)
